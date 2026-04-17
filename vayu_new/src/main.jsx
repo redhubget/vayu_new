b78c053673f7656027c15979eed36ff7faea3fcc
redhubget <ragureddy7412@gmail.com>
2026-04-17 14:27:17 +0530

    Update main.jsx

--- vayu_new/src/main.jsx
+++ vayu_new/src/main.jsx
@@ -1,8 +1,15 @@
+// This file is the starting point of your entire React app
+
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+// This enables page navigation without reloading
 import { BrowserRouter } from "react-router-dom";
+
+// Import your main App component
 import App from "./App.jsx";
 
+// This connects React to your HTML (index.html -> div id="root")
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
